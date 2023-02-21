e320ee8454c2f846d3310ed5af762b844e0132e3
Gion <69393137+gionsunday@users.noreply.github.com>
2023-02-21 16:52:29 +0100

    Add files via upload

---
+++ porfolio/js/sidebar.js
@@ -0,0 +1,14 @@
+
+     function closeSidebar(option){
+     const sidebar = document.getElementById('sidebar')
+     sidebar.style.display= 'none'
+     sidebar.style.transition= '0.7s'
+    
+    }
+function openSidebar() {
+     
+     const sidebar = document.getElementById('sidebar')
+     sidebar.style.display= 'block'
+     sidebar.style.transition= '0.7s'
+     
+}
